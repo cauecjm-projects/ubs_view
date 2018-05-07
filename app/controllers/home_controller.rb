47d643932d9d3a9a3c4f5954a91231c5cb0832a0
cauecjm <cauecjm@gmail.com>
2018-05-07 06:53:17 -0300

    home_controller: adicionando método 'buscar_ubs'

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,13 @@ class HomeController < ApplicationController
   def index
   end
 
+  def buscar_ubs
+    lat  = params['latitude']
+    long = params['longitude']
+
+    @places = Ubs.near([lat,long], 3)
+
+    render '/home/index'
+  end
+
 end
