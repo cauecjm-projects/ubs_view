65ef165b0540bb66bef01c198d05be431914b0b2
cauecjm <cauecjm@gmail.com>
2018-05-07 06:52:53 -0300

    models/ubs: adicionando configurações de geocoder

--- app/models/ubs.rb
+++ app/models/ubs.rb
@@ -4,4 +4,7 @@ class Ubs < ActiveRecord::Base
   validates_presence_of :cod_munic, :cod_cnes, :nom_estab
   validates :cod_munic, length: { in: 6..7 }
 
+  geocoded_by :address, latitude: :vlr_latitude, longitude: :vlr_longitude
+  after_validation :geocode, :if => :address_changed?
+
 end
